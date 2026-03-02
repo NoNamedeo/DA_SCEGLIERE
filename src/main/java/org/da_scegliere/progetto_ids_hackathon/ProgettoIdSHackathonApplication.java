27f3f2db47e4eb9ae12b5ce5b7e6b827f1a12923
NoNamedeo <meelalej@gmail.com>
2026-03-02 17:04:25 +0100

    prova

---
+++ src/main/java/org/da_scegliere/progetto_ids_hackathon/ProgettoIdSHackathonApplication.java
@@ -0,0 +1,13 @@
+package org.da_scegliere.progetto_ids_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoIdSHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoIdSHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/da_scegliere/progetto_ids_hackathon/ProgettoIdSHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.da_scegliere.progetto_ids_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoIdSHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
