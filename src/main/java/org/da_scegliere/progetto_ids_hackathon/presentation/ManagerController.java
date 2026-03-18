39f56cb7eb0db9d51cf83b4f750102fae135dc8f
matteo <matteo01.vittori@studenti.unicam.it>
2026-03-18 22:39:45 +0100

    controller aggiunti

---
+++ src/main/java/org/da_scegliere/progetto_ids_hackathon/presentation/ManagerController.java
@@ -0,0 +1,60 @@
+/*
+ * Authors:  Alejandro Innocenzi, Matteo Vittori, Vladislav Gaspari
+ * Copyright (c) 2026 Alejandro Innocenzi, Matteo Vittori, Vladislav Gaspari. All rights reserved.
+ *
+ * This file is part of the DA_SCEGLIERE project. Unauthorized copying,
+ * distribution, modification, or use of this file, via any medium,
+ * is strictly prohibited unless in compliance with the license.
+ *
+ * Licensed under the MIT License:
+ *     - Permission is hereby granted, free of charge, to any person obtaining
+ *       a copy of this software and associated documentation files (the "Software"),
+ *       to deal in the Software without restriction, including without limitation
+ *       the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *       and/or sell copies of the Software, and to permit persons to whom the
+ *       Software is furnished to do so, subject to the following conditions:
+ *
+ *     - The above copyright notice and this permission notice shall be included
+ *       in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package org.da_scegliere.progetto_ids_hackathon.presentation;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.da_scegliere.progetto_ids_hackathon.application.services.ManagerService;
+import org.da_scegliere.progetto_ids_hackathon.presentation.dto.request.SuspendUserRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/managers")
+public class ManagerController {
+
+    private final ManagerService managerService;
+
+    @PostMapping("/{managerId}/users/{userId}/suspensions")
+    public ResponseEntity<Void> suspendUser(
+            @PathVariable UUID managerId,
+            @PathVariable UUID userId,
+            @Valid @RequestBody SuspendUserRequest request
+    ) {
+        managerService.suspendUser(managerId, userId, request.reason());
+        return ResponseEntity.ok().build();
+    }
+}
